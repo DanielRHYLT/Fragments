0c95eff0b688d5fb8a3261a080737e9ae4c18f68
DanielR <20100771@ue.edu.pe>
2024-09-14 11:57:57 -0500

    Principal page and register

--- app/src/main/java/dev/dobler/fragments/fragments/RegistroFragment.kt
+++ app/src/main/java/dev/dobler/fragments/fragments/RegistroFragment.kt
@@ -5,7 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Adapter
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Spinner
+import android.widget.Toast
 import dev.dobler.fragments.R
+import java.util.Objects
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,43 +29,55 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class RegistroFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registro, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_registro, container, false)
+
+        val btnSave: Button = view.findViewById(R.id.btnSave)
+        val etFullName: EditText = view.findViewById(R.id.etFullName)
+        val etEmail: EditText = view.findViewById(R.id.etEmail)
+        val spCountry: Spinner = view.findViewById(R.id.spCountry)
+        val rgGender: RadioGroup = view.findViewById(R.id.rgGender)
+        val ckLicense: CheckBox = view.findViewById(R.id.ckLicense)
+        val ckCar: CheckBox = view.findViewById(R.id.ckCar)
+
+        //Loading Spinner
+        ArrayAdapter.createFromResource(requireContext(),
+            R.array.country_array,
+            android.R.layout.simple_spinner_item).also {
+                adapter -> adapter
+                    .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                spCountry.adapter = adapter
+        }
+        var spCountryValue = ""
+        spCountry.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
+                spCountryValue = parent?.getItemAtPosition(position).toString()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+        btnSave.setOnClickListener {
+            val fullNameValue = etFullName.text.toString()
+            val emailValue = etEmail.text.toString()
+            var intSelectedButton = rgGender!!.checkedRadioButtonId
+            var rbSelected: RadioButton = view.findViewById(intSelectedButton)
+            val genderValue = rbSelected.text
+
+            val allValues = "Full name: $fullNameValue \nEmail: $emailValue \nGender: $genderValue" +
+                            "\nCountry: $spCountryValue \nLicense: ${ckLicense.isChecked}" +
+                            "\nCar: ${ckCar.isChecked}"
+            Toast.makeText(requireContext(), allValues, Toast.LENGTH_LONG).show()
+        }
+
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegistroFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegistroFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
