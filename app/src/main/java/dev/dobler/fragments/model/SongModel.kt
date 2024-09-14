6fbbd94e8830e7846cfae5881abb24e7ae9e3f50
DanielR <20100771@ue.edu.pe>
2024-09-14 12:32:53 -0500

    PreSong

---
+++ app/src/main/java/dev/dobler/fragments/model/SongModel.kt
@@ -0,0 +1,9 @@
+package dev.dobler.fragments.model
+
+data class SongModel(
+    val id: Int,
+    val songName: String,
+    val album: String,
+    val countPlay: String,
+    val songTime: String
+)
